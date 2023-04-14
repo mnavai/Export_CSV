674dede42b3212ea43134c866462cb5fcf0e4caa
Maryam Navai <mnavai@Marys-MBP.lan>
2023-04-14 11:21:26 -0400

    csv created

--- index.js
+++ index.js
@@ -3,13 +3,38 @@ const { type } = require('os')
 
 
 function testFileRead(){    
-    fs.readFile('0912.TXT','utf8',(err,data)=>{
-                if(err){
-                    console.log(err)
-                    return
-                }
-                console.log(`The Shape of Data ${typeof data} \n The data as below : `)
-        });
+    const callBackFileRead = (err,data)=>{
+        if(err){
+            console.log(err)
+            return
+
+         }
+
+    //split data into rows
+    const rows = data.trim().split('\n')
+    
+    //create an array to hold the csv 
+    const csvData = []
+
+    //loop over each row and split data into columns
+    for (let i = 12 ; i < rows.length; i++){
+        const columns = rows[i].trim().split("\s+")
+        csvData.push(columns)
+    }
+
+    //convert csv into string 
+    const csvString = csvData.map(row => row.join(',')).join('\n');
+
+    //write csv string to the file
+    fs.writeFileSync('output.csv',csvString,(err) => {
+        if(err) throw err;
+        console.log("csv created")
+
+    });
+                //console.log(`The Shape of Data ${data} \n The data as below : `)
+    }
+
+     fs.readFile('0912.TXT', 'utf-8', callBackFileRead);
     
 }
 
