260acdc00c8c55a87d9b1889c508ecd50130b696
Maryam Navai <mnavai@Marys-MBP.lan>
2023-04-14 13:19:32 -0400

    final changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-function testFileRead(){    
+function testFileRead() {
   const callBackFileRead = (err, data) => {
     if (err) {
       console.log(err);
@@ -13,17 +13,29 @@ function testFileRead(){
     // Create an array to hold the CSV data
     const csvData = [];
 
-    // Loop through each row and split it into columns
+    // Loop through each row and extract the values of each column based on its width
     for (let i = 0; i < rows.length; i++) {
-      // Remove dashes and extra spaces from the row
-      const cleanedRow = rows[i].replace(/-/g, '').replace(/\s+/g, ' ').trim();
+      const values = [
+        rows[i].substring(0, 5).trim(),
+        rows[i].substring(5, 10).trim(),
+        rows[i].substring(10, 17).trim(),
+        rows[i].substring(17, 21).trim(),
+        rows[i].substring(21, 27).trim(),
+        rows[i].substring(27, 31).trim(),
+        rows[i].substring(31, 36).trim(),
+        rows[i].substring(36, 41).trim(),
+        rows[i].substring(41, 47).trim(),
+        rows[i].substring(47, 52).trim(),
+        rows[i].substring(52, 60).trim(),
+        rows[i].substring(60, 68).trim(),
+        rows[i].substring(68, 76).trim(),
+        rows[i].substring(76).trim(),
+      ];
 
-      // Skip empty or non-numeric rows
-      if (!cleanedRow || !/^\d/.test(cleanedRow)) continue;
+      // Skip rows that do not begin with a number
+      if (!/^\d/.test(values[0])) continue;
 
-      const columns = cleanedRow.split(' ');
-
-      csvData.push(columns);
+      csvData.push(values);
     }
 
     // Convert the CSV data to a string
